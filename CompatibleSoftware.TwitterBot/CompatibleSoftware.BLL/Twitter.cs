565090c1039275eab21211a2afe4881688fa081c
Kevin Main <kevmain@outlook.com>
2016-02-09 23:05:45 +0000

    Created soloution and first attempt at authorisation

---
+++ CompatibleSoftware.TwitterBot/CompatibleSoftware.BLL/Twitter.cs
@@ -0,0 +1,26 @@
+﻿using TweetSharp;
+
+namespace CompatibleSoftware.BLL
+{
+    public class Twitter
+    {
+        private readonly TwitterService _twitterService;
+
+        public Twitter()
+        {
+            _twitterService = new TwitterService("ConsumerKey", "SecretKey");
+            Authenticate();
+        }
+
+        private void Authenticate()
+        {
+            var uri = _twitterService.GetAuthorizationUri(_twitterService.GetRequestToken());
+        }
+
+
+        public void SendTweet(string message)
+        {
+            _twitterService.SendTweet(new SendTweetOptions());
+        }
+    }
+}
---
+++ CompatibleSoftware.TwitterBot/CompatibleSoftware.TwitterBot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CompatibleSoftware.BLL;
+
+namespace CompatibleSoftware.TwitterBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up...");
+
+            var twitter = new Twitter();
+            twitter.SendTweet("This is some text");
+
+            Console.WriteLine("Tweet Sent");
+
+            Console.ReadLine();
+        }
+    }
+}
